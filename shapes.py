16502d268ebf04d067396461a22e41bf8c20e88d
Robert Gillis <rgillis873@gmail.com>
2023-04-04 08:30:52 -0400

    Circles

--- shapes.py
+++ shapes.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("draw2.png")
+img = cv2.imread("sailboat.png")
 gray_scale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur_img = cv2.GaussianBlur(gray_scale_img, (3,3), 1,1)
 ret, black_white_thresholded_img = cv2.threshold(blur_img, 1, 255, cv2.THRESH_BINARY)
 
 contours, hierarchy = cv2.findContours(black_white_thresholded_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-circles = cv2.HoughCircles(black_white_thresholded_img, cv2.HOUGH_GRADIENT, 1, black_white_thresholded_img.shape[0]/8, param1=100, param2=30, minRadius=1, maxRadius=100)
+circles = cv2.HoughCircles(black_white_thresholded_img, cv2.HOUGH_GRADIENT, 1, 100,  param1=100, param2=30, minRadius=1, maxRadius=200)
+#circles = cv2.HoughCircles(black_white_thresholded_img, cv2.HOUGH_GRADIENT, 1, black_white_thresholded_img.shape[0]/8, param1=100, param2=30, minRadius=1, maxRadius=200)
+
 
 for contour in contours:
     approx_curve = cv2.approxPolyDP(contour, 3, True)
@@ -35,6 +37,12 @@ for contour in contours:
         #    cv2.putText(img, "Rectangle", (rect[0]+(rect[2]//2), rect[1]+(rect[3]//2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
         #else:
             #cv2.putText(img, "Square", (rect[0]+(rect[2]//2), rect[1]+(rect[3]//2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
+
+if circles is not None:
+    circles = np.round(circles[0,:]).astype("int")
+    for i in range(0, len(circles)):
+        cv2.circle(img, (circles[i][0], circles[i][1]), circles[i][2], (0,255,255), 3)  
+        cv2.putText(img, "Circle",(circles[i][0], circles[i][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255))  
 
 
 
